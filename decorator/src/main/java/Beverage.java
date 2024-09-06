1dffabc6767e79f219c6f60f77c0bad92ca19738
miniato2 <whgg456@gmail.com>
2024-09-06 23:23:57 +0900

    feat : decorator

--- decorator/src/main/java/Beverage.java
+++ decorator/src/main/java/Beverage.java
@@ -1,3 +1,9 @@
-public class Beverage {
-    private String description;
+public abstract class Beverage {
+    String description;
+
+    public String getDescription(){
+        return description;
+    }
+
+    public abstract double cost();
 }
---
+++ decorator/src/main/java/CondimentDecorator.java
@@ -0,0 +1,7 @@
+public abstract class CondimentDecorator extends Beverage {
+    //첨가물 추상 클래스
+
+    Beverage beverage;
+
+    public abstract String getDescription();
+}
